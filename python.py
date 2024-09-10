1c98da8861e404f2e7bcb16d081ce9b9cdb4043f
Patrick <patrickaasamoah@gmail.com>
2024-09-10 01:55:57 -0500

    file

--- python.py
+++
@@ -1,8 +0,0 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = [x for x in fruits if "a" in x]
-
-""" for x in fruits:
-    if "a" in x:
-        newlist.append(x)
- """
-print(newlist)
